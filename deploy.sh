ec18ad60a42b6121c386e897b0efa55a61d2e21e
Loïc VDB <l.vandenbossche.jouvet@gmail.com>
2026-01-07 22:38:49 +0100

    Try new disks paths

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 COMPOSE_FILE="docker-compose.prod.yml"
-HOST_DOWNLOADS_PATH="/mnt"
+HOST_DOWNLOADS_PATH="./data/library"
 
 echo "Building images with ${COMPOSE_FILE}..."
 HOST_DOWNLOADS_PATH="${HOST_DOWNLOADS_PATH}" docker compose -f "${COMPOSE_FILE}" build
